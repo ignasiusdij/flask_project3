ac1cf7dade0b83ada7f8d8aabba80efcd4e3da9f
ignasiusdij <indjaya98@yahoo.co.id>
2020-02-15 15:53:11 +0700

    First Commit all

--- forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
-
-class NameForm(FlaskForm):
-    name = StringField('Whats is your name?', validators=[Required()])
-    submit = SubmitField('Submit')
